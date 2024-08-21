ff3dab1f852a75b493aac3e820a84877996f181b
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-22 00:56:19 +0530

    db initi

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const dbConnect = () =>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then( () => {
+        console.log("DB connection isd Successfull")
+    })
+    .catch((error) =>{
+        console.log("DB is facing Connection Issues")
+        console.log(error.message)
+        process.exit(1);
+    })
+};
+
+module.exports = dbConnect;
