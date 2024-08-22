b03a31208ae0d468531b2e124c6ccccb368fdc53
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-23 01:23:20 +0530

    controller logic

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -9,9 +9,10 @@ exports.localFileUpload = async (req, res) => {
         // This line extracts the uploaded file from the req (request) object.
         console.log(file);
 
-        let path = __dirname + "/files/" + Date.now();
+        let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
+        // `.${file.name.split('.')[1]}` this is used to extract the extention of the file that you are uploading 
         // let path: This declares a variable path to store the location where the file will be saved.
-        // __dirname: This is a Node.js global variable that gives you the direactory name of the current folder you are in (i.e.the folder where the file is located)
+        // __dirname: This is a Node.js global variable that gives you the direactory name of the current folder you are in (i.e., the folder where this fileUpload script is located).
         // "/files/": This is the folder inside the current directory where you want to save the uploaded file.
         // Date.now(): This generates a unique timestamp representing the current time in milliseconds. It's used here to create a unique file name, reducing the risk of overwriting existing files.
         // __dirname + "/files/" + Date.now();: Combines these pieces to create the full path where the file will be saved. For example, it might produce something like /your-project-directory/files/1627382930123.
