cb28b100cef884dc7531750fd37d0297ea98d834
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-24 02:14:20 +0530

    videoUpload route

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {imageUpload, localFileUpload } = require("../controllers/fileUpload")
+const {imageUpload, localFileUpload, videoUpload} = require("../controllers/fileUpload")
 
 // api routes
 router.post("/localFileUpload", localFileUpload);
 router.post("/imageUpload", imageUpload);
+router.post("/videoUpload", videoUpload);
 
 // exporting the router
 module.exports = router;
