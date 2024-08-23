ce3821917c70d3828ed0464196a5fac52bbc3c3e
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-24 00:59:47 +0530

    added imageUpload route

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {imageUpload, videoUpload, imageReducerUpload, localFileUpload } = require("../controllers/fileUpload")
+const {imageUpload, localFileUpload } = require("../controllers/fileUpload")
 
 // api routes
 router.post("/localFileUpload", localFileUpload);
+router.post("/imageUpload", imageUpload);
 
 // exporting the router
 module.exports = router;
