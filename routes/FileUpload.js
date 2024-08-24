88dbec6db80509c3ccebe8cc25f7d904ce2b33c8
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-24 15:27:14 +0530

    imageSizeReducer route

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {imageUpload, localFileUpload, videoUpload} = require("../controllers/fileUpload")
+const {imageUpload, localFileUpload, videoUpload, imageSizeReducer} = require("../controllers/fileUpload")
 
 // api routes
 router.post("/localFileUpload", localFileUpload);
 router.post("/imageUpload", imageUpload);
 router.post("/videoUpload", videoUpload);
+router.post("/imageSizeReducer", imageSizeReducer);
 
 // exporting the router
 module.exports = router;
